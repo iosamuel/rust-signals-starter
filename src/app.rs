35c0dae8234381864a7b76ee3becce5ad6348d63
Samuel Burbano <me@iosamuel.dev>
2023-02-23 12:07:11 -0500

    feat: signals in rust ⚡

--- src/app.rs
+++ src/app.rs
@@ -22,6 +22,7 @@ pub fn App(cx: Scope) -> impl IntoView {
             <main>
                 <Routes>
                     <Route path="" view=|cx| view! { cx, <ColorPage/> }/>
+                    <Route path="/read-time" view=|cx| view! { cx, <ReadTime/> }/>
                 </Routes>
             </main>
         </Router>
@@ -72,5 +73,49 @@ fn ColorPage(cx: Scope) -> impl IntoView {
     view! { cx,
         <h1 style=styles>"Color Page"</h1>
         <input type="color" value=color on:change=move |e| change_color(event_target_value(&e)) />
+    }
+}
+
+/// Renders the read time page
+#[component]
+fn ReadTime(cx: Scope) -> impl IntoView {
+    const STANDARD_WPM: u8 = 200;
+
+    let (text, set_text) = create_signal(cx, String::new());
+    let word_count = move || text().split_whitespace().count() as u8;
+    let read_time = move || {
+        let time = word_count() as f32 / STANDARD_WPM as f32;
+        let minutes = time.floor() as u8;
+        let seconds = ((time - minutes as f32) * 60.0).round() as u8;
+
+        if minutes == 0 && seconds == 0 {
+            return "0s".to_string();
+        }
+
+        let mut minutes_str = minutes.to_string();
+        let mut seconds_str = seconds.to_string();
+
+        if minutes < 10 {
+            minutes_str = format!("0{}", minutes)
+        }
+        if seconds < 10 {
+            seconds_str = format!("0{}", seconds)
+        }
+
+        format!("{}:{}", minutes_str, seconds_str)
+    };
+
+    create_effect(cx, move |_| {
+        log!("changed {}", read_time());
+    });
+
+    view! { cx,
+        <h1>{read_time} " read time - Words " {word_count}</h1>
+        <textarea
+        rows="10"
+        cols="50"
+        value={text()}
+        on:input=move |e| set_text(event_target_value(&e))
+      ></textarea>
     }
 }
